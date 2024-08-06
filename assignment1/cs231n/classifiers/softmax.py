1f19a4291e1c8e4c44d0c2b82a8f3bdf5f6f2e6d
StanislavGerasimov <149607275+StanislavGerasimov@users.noreply.github.com>
2024-08-06 20:16:30 +0300

    Modified linear_classifier file

--- assignment1/cs231n/classifiers/softmax.py
+++ assignment1/cs231n/classifiers/softmax.py
@@ -26,40 +26,6 @@ def softmax_loss_naive(W, X, y, reg):
     loss = 0.0
     dW = np.zeros_like(W)
 
-    N = X.shape[0]
-    C = W.shape[1]
-
-    for i in range(N):
-        # Compute the scores
-        scores = X[i].dot(W)
-        
-        # Stabilize the scores for numerical stability
-        max_score = np.max(scores)
-        exp_scores = np.exp(scores - max_score)
-        
-        # Compute the softmax probabilities
-        probs = exp_scores / np.sum(exp_scores)
-        
-        # Compute the loss
-        loss += -np.log(probs[y[i]])
-        
-        # Compute the gradient
-        for j in range(C):
-            if j == y[i]:
-                dW[:, j] += (probs[j] - 1) * X[i]
-            else:
-                dW[:, j] += probs[j] * X[i]
-
-    # Average loss and gradient
-    loss /= N
-    dW /= N
-
-    # Add regularization to the loss
-    loss += reg * np.sum(W * W)
-
-    # Add regularization to the gradient
-    dW += 2 * reg * W
-
     #############################################################################
     # TODO: Compute the softmax loss and its gradient using explicit loops.     #
     # Store the loss in loss and the gradient in dW. If you are not careful     #
@@ -68,7 +34,7 @@ def softmax_loss_naive(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    
+    pass
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
@@ -85,47 +51,6 @@ def softmax_loss_vectorized(W, X, y, reg):
     loss = 0.0
     dW = np.zeros_like(W)
 
-    # Compute the scores
-    scores = X.dot(W)
-    '''
-    # Stabilize the scores for numerical stability
-    scores -= np.max(scores, axis=1, keepdims=True)
-    
-    # Compute the softmax probabilities
-    exp_scores = np.exp(scores)
-    probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
-    
-    # Compute the loss
-    correct_log_probs = -np.log(probs[np.arange(X.shape[0]), y])
-    loss = np.sum(correct_log_probs) / X.shape[0]
-    loss += reg * np.sum(W * W)
-    
-    # Compute the gradient
-    dscores = probs
-    dscores[np.arange(X.shape[0]), y] -= 1
-    dW = X.T.dot(dscores) / X.shape[0]
-    dW += 2 * reg * W
-
-    return loss, dW
-    '''
-     # Shift the scores for numerical stability
-    scores -= np.max(scores, axis=1, keepdims=True)
-
-    # Compute the class probabilities
-    softmax_output = np.exp(scores) / np.sum(np.exp(scores), axis=1, keepdims=True)
-
-    # Compute the loss: average cross-entropy loss and regularization
-    correct_class_probabilities = softmax_output[range(X.shape[0]), y]
-    loss = -np.sum(np.log(correct_class_probabilities)) / X.shape[0]
-    loss += 0.5 * reg * np.sum(W * W)
-
-    # Compute the gradient on scores
-    softmax_output[range(X.shape[0]), y] -= 1
-    dW = X.T.dot(softmax_output) / X.shape[0]
-    dW += reg * W
-
-    return loss, dW
-
     #############################################################################
     # TODO: Compute the softmax loss and its gradient using no explicit loops.  #
     # Store the loss in loss and the gradient in dW. If you are not careful     #
@@ -134,8 +59,8 @@ def softmax_loss_vectorized(W, X, y, reg):
     #############################################################################
     # *****START OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-   
+    pass
 
     # *****END OF YOUR CODE (DO NOT DELETE/MODIFY THIS LINE)*****
 
-    
+    return loss, dW
